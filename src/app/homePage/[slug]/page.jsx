7b15496c83fb01a854477a60f241bf1bb364539d
MAHEDI HASAN <mahedi5096@gmail.com>
2026-04-16 19:25:43 +0600

    card details added

--- src/app/homePage/[slug]/page.jsx
+++ src/app/homePage/[slug]/page.jsx
@@ -1,11 +1,141 @@
-import React from 'react';
+import contacts from "@/data/contacts.json";
+import Image from "next/image";
+import { AiTwotoneDelete } from "react-icons/ai";
+import { BiAlarmSnooze, BiPhoneCall } from "react-icons/bi";
+import { LuFolderArchive } from "react-icons/lu";
+import { MdOutlineTextsms } from "react-icons/md";
+import { PiVideoCameraBold } from "react-icons/pi";
 
-const addFriend = () => {
+const ContactDetails = async ({ params }) => {
+
+    const { slug } = await params;
+
+    const contact = contacts.find(
+        (c) => c.id === Number(slug)
+    );
+
+    const statusStyle = {
+        "overdue": "badge badge-error",
+        "almost due": "badge badge-info",
+        "on-track": "badge badge-success",
+    };
+
+    if (!contact) {
+        return <p className="text-center mt-10">Contact not found</p>;
+    }
+
     return (
-        <div>
-            <h1>add friend here</h1>
+        <div className='px-32 py-10 grid grid-cols-12 gap-6'>
+
+            <div className='col-span-4 space-y-4'>
+
+                <div className="card bg-base-100 shadow p-6 text-center">
+
+                    <Image
+                        src={contact.picture}
+                        alt={contact.name}
+                        width={64}
+                        height={64}
+                        className="rounded-full mx-auto h-16 w-16"
+                    />
+
+                    <h2 className='font-bold text-lg mt-3'>
+                        {contact.name}
+                    </h2>
+
+                    <p className="text-sm text-gray-500">
+                        {contact.email}
+                    </p>
+
+                    <div className="flex gap-4 mx-auto">
+                        <div className={`${statusStyle[contact.status]} mt-2`}>
+                            {contact.status}
+                        </div>
+
+                        <div className='badge badge-outline mt-2'>
+                            {contact.tags[0]}
+                        </div>
+                    </div>
+
+                    <p className='italic text-sm mt-2'>
+                        {contact.bio}
+                    </p>
+                </div>
+
+                <button className='btn w-full'><BiAlarmSnooze />Snooze 2 Weeks</button>
+                <button className='btn w-full'><LuFolderArchive /> Archive</button>
+                <button className='btn w-full btn-error'><AiTwotoneDelete /> Delete</button>
+            </div>
+
+           
+            <div className='col-span-8 space-y-6'>
+
+                <div className='grid grid-cols-3 gap-4 '>
+                    <div className="card bg-base-100 shadow text-center p-6">
+                        <h2 className='text-3xl font-bold'>
+                            {contact.days_since_contact}
+                        </h2>
+                        <p>Days Since Contact</p>
+                    </div>
+
+                    <div className="card bg-base-100 shadow text-center p-6">
+                        <h2 className='text-3xl font-bold'>
+                            {contact.goal}
+                        </h2>
+                        <p>Goal (Days)</p>
+                    </div>
+
+                    <div className="card bg-base-100 shadow text-center p-6">
+                        <h2 className='text-xl font-bold'>
+                            {contact.next_due_date}
+                        </h2>
+                        <p>Next Due</p>
+                    </div>
+                </div>
+
+                <div className="card bg-base-100 shadow p-7">
+                    <div className="flex justify-between">
+                        <h2 className='font-semibold text-lg'>Relationship Goal</h2>
+                        <button className="btn">Edit</button>
+                    </div>
+                    <p>
+                        Connect every <span className='font-bold'>
+                            {contact.goal} days
+                        </span>
+                    </p>
+                </div>
+
+                <div className="bg-base-200 shadow p-4">
+                    <h3>Quick Check-In</h3>
+                    <div className='grid grid-cols-3 gap-4 pt-1'>
+                    <div className="card bg-base-100 shadow text-center p-4 flex flex-col items-center justify-center">
+                        <h2 className='text-3xl font-bold'>
+                            <BiPhoneCall />
+                        </h2>
+                        <p>call</p>
+                    </div>
+
+                    <div className="card bg-base-100 shadow text-center p-4 flex flex-col items-center justify-center">
+                        <h2 className='text-3xl font-bold'>
+                           <MdOutlineTextsms />
+                        </h2>
+                        <p>Text</p>
+                    </div>
+
+                    <div className="card bg-base-100 shadow text-center flex flex-col items-center justify-center p-4">
+                        <h2 className='text-3xl font-bold'>
+                           <PiVideoCameraBold />
+                        </h2>
+                        <p>Video</p>
+                    </div>
+                </div>
+
+
+                </div>
+
+            </div>
         </div>
     );
 };
 
-export default addFriend;
+export default ContactDetails;
--- src/app/homePage/page.jsx
+++ src/app/homePage/page.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
 import contacts from "@/data/contacts.json";
 import Image from 'next/image';
+import Link from 'next/link';
+
 
 const HomePage = () => {
     return (
-        <div className='px-44 py-16'>
+        <div className='px-32 py-16'>
             <div className='text-center'>
                 <h1 className='font-bold text-5xl'>Friends to keep close in your life</h1>
                 <p className='font-normal text-lg py-4'>Your personal shelf of meaningful connections. Browse, tend, and nurture the
                     relationships that matter most.
                 </p>
-                <button className='text-white bg-green-700 p-2 rounded-sm'>+ Add a Friend</button>
-                <div className='grid grid-cols-4 py-8'>
-                    <div className="card bg-base-100 w-48 shadow-sm text-center col-span-1">
+                <button className='text-white bg-green-700 p-2 rounded-md'>+ Add a Friend</button>
+                <div className='grid grid-cols-4 py-8 gap-4'>
+                    <div className="card bg-base-100  shadow-sm text-center col-span-1">
                         <div className="card-body">
                             <h2 className='font-semibold text-3xl'>10</h2>
                             <p className='font-normal text-base'>Total Friends</p>
                         </div>
                     </div>
-                    <div className="card bg-base-100 w-48 shadow-sm text-center col-span-1">
+                    <div className="card bg-base-100  shadow-sm text-center col-span-1">
                         <div className="card-body">
                             <h2 className='font-semibold text-3xl'>3</h2>
                             <p className='font-normal text-base'>On Track</p>
                         </div>
                     </div>
-                    <div className="card bg-base-100 w-48 shadow-sm text-center col-span-1">
+                    <div className="card bg-base-100  shadow-sm text-center col-span-1">
                         <div className="card-body">
                             <h2 className='font-semibold text-3xl'>6</h2>
                             <p className='font-normal text-base'>Need Attention</p>
                         </div>
                     </div>
-                    <div className="card bg-base-100 w-48 shadow-sm text-center col-span-1">
+                    <div className="card bg-base-100  shadow-sm text-center col-span-1">
                         <div className="card-body">
                             <h2 className='font-semibold text-3xl'>12</h2>
                             <p className='font-normal text-base'>Interactions This Month</p>
@@ -44,17 +46,37 @@ const HomePage = () => {
                 <h1>Your Friends</h1>
                 <div className="grid grid-cols-4 gap-4">
                     {contacts.map((c) => (
-                        <div key={c.id} className="p-4 border rounded items-center text-center">
-                            <h2 className="font-bold">{c.name}</h2>
-                            <p>{c.email}</p>
-                            <Image
-                                src={c.picture}
-                                alt={c.name}
-                                width={100}
-                                height={100}
-                                className="rounded-full"
-                            />
-                        </div>
+                        <Link key={c.id} href={`/homePage/${c.id}`}>
+                            <div className="p-4 rounded shadow-md text-center">
+                                <Image
+                                    src={c.picture}
+                                    alt={c.name}
+                                    width={60}
+                                    height={60}
+                                    className="rounded-full mx-auto h-16 w-16"
+                                />
+                                <h2 className="font-bold">{c.name}</h2>
+                                <p>{c.days_since_contact}d ago</p>
+                                <div className='flex gap-2 text-center py-2 mx-auto'>
+                                    <p className='badge badge-accent'>{c.tags[0]}</p>
+                                    <p className=' bg-teal-400 rounded-lg'>{c.tags[1]}</p>
+                                </div>
+
+                                <p
+                                    className={`
+                                         ${c.status === "overdue" ? "badge badge-error" : ""}
+                                         ${c.status === "almost due" ? "badge badge-info" : ""}
+                                         ${c.status === "on-track" ? "badge badge-warning" : ""}
+                                         `}
+                                >
+                                    {c.status}
+                                </p>
+
+
+                            </div>
+                        </Link>
+
+
                     ))}
                 </div>
             </div>
